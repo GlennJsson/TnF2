1b45e420241ba276f8c7ca5a1d0b1d1829da92d0
glejoh0122 <glejoh0122@BA5CG1444LZG>
2025-01-16 14:17:42 +0100

    Add project files.

---
+++ TnF2/CodeFile1.cs
@@ -0,0 +1,57 @@
+﻿/*Christers kod
+
+
+//Läs Qualisys eltidsfil (.txt)
+using StreamReader sr = new(strPath, Encoding.UTF8);
+string strWind = "";
+string? strLine = sr.ReadLine();
+for (int i = 0; i < 11; i++)
+{
+    if (strLine != string.Empty && strLine != null)
+    {
+        if (strLine.Substring(0, 5) == "Wind:")
+        {
+            strWind = strLine.Substring(5, 4).Trim();
+        }
+        if (strLine.Substring(0, 4) == "Rank" || strLine.Substring(0, 4) == "Plac")
+        {
+            using MySqlConnection con1 = new(_configuration.GetConnectionString("ConnTfdb"));
+            {
+                string strSql1 = "sp_resIn_Time_import"; //Använd Stored Procedure -      
+                using MySqlCommand cmd1 = new(strSql1, con1);
+                cmd1.CommandType = CommandType.StoredProcedure;
+
+                string? strResline = sr.ReadLine();
+                while (strResline != null)
+                {
+                    string[] strData = strResline.Split('\t');
+                    string strRank = strData[0].Trim();
+                    string strLane = strData[1].Trim();
+                    string strBib = strData[2].Trim();
+                    string strTime = strData[3].Trim();
+
+                    cmd1.Parameters.Clear();
+                    cmd1.Parameters.AddWithValue("@compId", intCompId);
+                    cmd1.Parameters.AddWithValue("@eventId", intEventId);
+                    cmd1.Parameters.AddWithValue("@evNum", intEventNum);
+                    cmd1.Parameters.AddWithValue("@roundId", intRoundId);
+                    cmd1.Parameters.AddWithValue("@heatId", intHeatId);
+                    cmd1.Parameters.AddWithValue("@bib", strBib);
+                    cmd1.Parameters.AddWithValue("@lane", strLane);
+                    cmd1.Parameters.AddWithValue("@rank", strRank);
+                    cmd1.Parameters.AddWithValue("@time", strTime);
+                    cmd1.Parameters.AddWithValue("@wind", strWind);
+
+                    con1.Open();
+                    cmd1.ExecuteNonQuery();
+                    con1.Close();
+
+                    strResline = sr.ReadLine();
+                }
+            }
+        }
+    }
+    strLine = sr.ReadLine();
+}
+}
+*/
---
+++ TnF2/Program.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading;
+
+namespace TnF2
+{
+    
+    internal class Program
+    {
+        // Defaults
+        static string strPath = @".\";
+        static int currentCheckSum = 0;
+        static FileSystemWatcher fsw;
+
+
+        // Skapa hash på [filnamn + storlek + skapad tid] för att komma runt "buggar" i komponenten FileSystemWatcher.
+        static int CalculateChecksum(string input)
+        {
+            var _f = new FileInfo(input);
+
+            string _s = _f.FullName + _f.Length.ToString() + _f.CreationTime.ToString();
+
+            Byte[] _b = Encoding.UTF8.GetBytes(_s);
+
+            //Console.WriteLine(_s);
+
+            int sum = 0;
+            foreach (Byte c in _b)
+            {
+                sum += c; // Add ASCII value of each character
+            }
+            //Console.WriteLine(sum);
+
+            return sum;
+        }
+
+        static void PrintHelpAndExit()
+        {
+            Console.WriteLine("  Commandline arguments:");
+            Console.WriteLine("  ======================");
+            Console.WriteLine("  -h : Prints this help.");
+            Console.WriteLine("  -p : Set working directory for filewatch. (E.g. .\\Dir\\ or full path).");
+            Console.WriteLine("");
+            System.Environment.Exit(0);
+        }
+
+        static void GetArgs(string[] args)
+        {
+            
+            for (uint i=0; i < args.Length; i++)
+            {
+                switch (args[i])
+                {
+                    case "-h":
+                        PrintHelpAndExit();
+                        break;
+                    case "-p":
+                        if(i < (args.Length - 1))
+                        {
+                            i++;
+                            strPath = args[i];
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                GetArgs(args);
+            }
+
+            fsw = new FileSystemWatcher(strPath);
+
+            Console.Out.WriteLine("Press enter to exit.");
+            Console.WriteLine("Reading directory: {0}", strPath);
+
+            WatchIt();
+
+            _ = Console.ReadLine();
+        }
+
+        static void WatchIt()
+        {
+            
+
+            fsw.NotifyFilter = NotifyFilters.Attributes
+                                 | NotifyFilters.CreationTime
+                                 | NotifyFilters.DirectoryName
+                                 |  NotifyFilters.FileName
+                                 //| NotifyFilters.LastAccess
+                                 | NotifyFilters.LastWrite
+                                 | NotifyFilters.Security
+                                 //| NotifyFilters.Size
+                                 ;
+                                
+            fsw.Changed += OnChanged;
+            fsw.Created += OnCreated;
+            fsw.Deleted += OnDeleted;
+            fsw.Renamed += OnRenamed;
+            fsw.Error += OnError;
+
+            fsw.Filter = "*.txt";
+            fsw.IncludeSubdirectories = false;
+            fsw.EnableRaisingEvents = true;
+
+        }
+
+        private static void OnChanged(object sender, FileSystemEventArgs e)
+        {
+            int _cs = CalculateChecksum(e.FullPath);
+            if (_cs.CompareTo(currentCheckSum) != 0)
+            {
+                Console.WriteLine($"Changed: {e.Name} :: [{_cs}]");
+                currentCheckSum = _cs;
+            }
+        }
+
+        private static void OnCreated(object sender, FileSystemEventArgs e)
+        {
+            fsw.EnableRaisingEvents = false;
+            Thread.Sleep(500);
+            currentCheckSum = CalculateChecksum(e.FullPath);
+
+            string value = $"Created: {e.Name} :: :: [{currentCheckSum}]";
+            Console.WriteLine(value);
+            fsw.EnableRaisingEvents = true;
+        }
+
+        private static void OnDeleted(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine($"Deleted: {e.Name}");
+        }
+            
+
+        private static void OnRenamed(object sender, RenamedEventArgs e)
+        {
+            int _cs = CalculateChecksum(e.FullPath);
+            if (_cs != currentCheckSum)
+            {
+                Console.WriteLine($"Renamed:");
+                Console.WriteLine($"    Old: {e.OldFullPath}");
+                Console.WriteLine($"    New: {e.FullPath} :: [{_cs}]");
+                currentCheckSum = _cs;
+            }
+        }
+
+        private static void OnError(object sender, ErrorEventArgs e) =>
+            PrintException(e.GetException());
+
+        private static void PrintException(Exception ex)
+        {
+            if (ex != null)
+            {
+                Console.WriteLine($"Message: {ex.Message}");
+                Console.WriteLine("Stacktrace:");
+                Console.WriteLine(ex.StackTrace);
+                Console.WriteLine();
+                PrintException(ex.InnerException);
+            }
+        }
+    }
+}
